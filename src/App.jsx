c3fd81d40275eb9538c439227919c481cc25d00f
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-11 16:44:06 +0100

    ADD axios and fetch logic in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,39 @@ import Home from "./pages/Home";
 import Show from "./pages/posts/Show";
 import Index from "./pages/posts/Index";
 import Create from "./pages/posts/Create";
+import axios from "axios";
 
 import "./App.css";
 import ErrorNotFound from "./pages/ErrorNotFound/ErrorNotFound";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GlobalContext from "./Context/GlobalContext";
 
 const API_BASE_URI = "http://localhost:3000/";
 
 function App() {
   const [categories] = useState(["Dolci", "Primi", "Secondi"]);
+  const [posts, setPosts] = useState([]);
+
+  function fetchPosts() {
+    axios
+      .get(`${API_BASE_URI}posts`)
+
+      .then((res) => {
+        setPosts(res.data.posts);
+        console.log(res.data.posts);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
     <>
-      <GlobalContext.Provider value={{ categories, API_BASE_URI }}>
+      <GlobalContext.Provider value={{ categories, API_BASE_URI, posts }}>
         <BrowserRouter>
           <Navbar />
           <Routes>
